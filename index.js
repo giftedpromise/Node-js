eced952d24503fd76517360873db7460a573eb3d
giftedpromise <giftedpromise1992@gmail.com>
2024-10-01 00:13:29 +0100

    node

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+/*
+Day 1: Practice Task: Create a New Project
+1. Open your terminal.
+
+2. Create a new directory for your project:
+mkdir node-basics
+cd node-basics
+
+3. Run npm init to initialize the project:
+npm init -y
+
+4. Install a package (e.g., chalk for coloring terminal text):
+
+*/
+
 import chalk from "chalk";
 console.log(chalk.green("Hello, Node.js!"));
