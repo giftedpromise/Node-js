0bfaa38c429a36fe1899612c8e58c1ad1ef43c75
giftedpromise <giftedpromise1992@gmail.com>
2024-10-03 17:43:29 +0100

    node

---
+++ modules.js
@@ -0,0 +1,23 @@
+/*
+4. HTTP Module
+The http module allows Node.js to create a web server. It can handle requests and send responses.
+
+Common Methods:
+http.createServer(): Creates an HTTP server instance.
+server.listen(): Makes the server listen for incoming requests.
+Example: Creating a Basic HTTP Server
+
+*/
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, World!\n");
+});
+
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
+
+const fs = require("fs");
